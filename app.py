75433c38ce04941e46d4c9b2ef6ec6f12314fd2f
AbdulRahman Eldeeb <abdodebo3@gmail.com>
2024-10-18 11:08:51 +0000

    token length solved

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import pipeline
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 # Define the summarization function
-def summarize_text(article):
+def summarize_text(article[:1024]):
     summary = summarizer(article, max_length=130, min_length=30, do_sample=False)
     return summary[0]['summary_text']
 
